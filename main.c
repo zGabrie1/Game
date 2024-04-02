304fa53b05d15f1a4f5cb36837e1e895e5990841
SoaresGabriel <gabriel980500@gmail.com>
2024-04-01 21:16:20 -0300

    sd

--- main.c
+++ main.c
@@ -37,11 +37,6 @@ int main(void)
         // TODO: Update your variables here
         //--------------------------------------------------------------------------------
 
-        if (IsKeyDown(KEY_RIGHT)) player1.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) player1.x -= 2.0f;
-        if (IsKeyDown(KEY_UP)) player1.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) player1.y += 2.0f;
-
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
